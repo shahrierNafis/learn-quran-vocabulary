acd76b7f5f056ee6f19f67a6fa80a68dddda4429
shahrierNafis <102902752+shahrierNafis@users.noreply.github.com>
2024-03-01 19:24:43 +0600

    Refactor: main page to implement OTP login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
+
 import { createClient } from "@/utils/supabase/clients";
 import { useEffect, useState } from "react";
+
+import { OTPInput } from "input-otp";
+import SendOTP from "@/components/SendOTP";
+import Slot from "@/components/ui/Slot";
+
 export default function Home() {
   const supabase = createClient();
   const [isLoading, setIsLoading] = useState(true);
+  const [isSent, setIsSent] = useState(false);
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     supabase.auth.getUser().then(({ data: { user }, error }) => {
@@ -16,21 +22,47 @@ export default function Home() {
     });
   }, []);
 
+  function handleOTPInput(token: any) {
+    supabase.auth
+      .verifyOtp({ email, token, type: "email" })
+      .then(({ error }) => {
+        if (error) {
+          alert(error.message);
+        } else {
+          window.location.href = "/dashboard";
+        }
+      });
+  }
+
   return (
     <>
-      {isLoading ? (
-        <></>
-      ) : (
-        <Auth
-          supabaseClient={supabase}
-          view="magic_link"
-          appearance={{ theme: ThemeSupa }}
-          theme="dark"
-          showLinks={false}
-          providers={[]}
-          redirectTo="http://localhost:3000/auth/callback"
-        />
-      )}
+      <div className="flex flex-col w-full h-[100dvh] justify-center items-center gap-2">
+        {isLoading ? (
+          <></>
+        ) : !isSent ? (
+          <>
+            <SendOTP {...{ setIsSent, setEmail }} />
+          </>
+        ) : (
+          <>
+            <OTPInput
+              onComplete={handleOTPInput}
+              maxLength={6}
+              containerClassName="group flex items-center has-[:disabled]:opacity-30"
+              render={({ slots }) => (
+                <div className="flex flex-col">
+                  <div className="text-xl block font-bold">Enter OTP:</div>
+                  <div className="flex">
+                    {slots.map((slot, idx) => (
+                      <Slot key={idx} {...slot} />
+                    ))}
+                  </div>
+                </div>
+              )}
+            />
+          </>
+        )}
+      </div>
     </>
   );
 }
